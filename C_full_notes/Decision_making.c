df9a6e96865af6970c64a19d79cb6bc977f7a6e2
MD SLAUDDIN <88940044+Mdslauddin@users.noreply.github.com>
2022-04-24 18:00:56 +0530

    Add files via upload

---
+++ C_full_notes/Decision_making.c
@@ -0,0 +1,111 @@
+#include<stdio.h>
+
+int main()
+{
+    // Decision making 
+    
+    /*
+     if statements
+    
+    int a=5; //6
+    int b=6; //3
+    if(a>b){
+    
+    printf("Condtion is true");
+    }
+    
+    */
+    
+    
+    // A Greater program check 
+  /*  int a;
+    int b;
+    printf("enter integer no : ");
+    scanf("%d",&a);
+    printf("enter second no : ");
+    scanf("%d",&b);
+    if(a>b){
+    printf(" A Greater then ");
+    
+    }
+    
+    */
+    
+    
+    
+  // if and else 
+
+  /* int a=5;//7
+  int b=6;//6
+  if(a>b){
+  printf("A is Greater ");
+
+}  else{
+
+ printf("B is Greater ");
+
+}
+   */
+
+
+
+// Nested if 
+/*  
+ int a=7; // 7
+   int b=6; //9
+  int c=8; //4
+  if ( a>b && a>c){
+  printf("A is Greater");
+
+}
+else if(b>a && b>c){
+printf("B is Greater ");
+
+}else{
+
+printf("C is GReater ");
+}
+*/
+ // Q even odd
+// Q Leap year 
+// Q five subject mark
+// Q total % and grate
+
+ // Even odd program 
+/*
+ int no ;
+printf("Enter any no : ");
+scanf("%d",&no);
+if(no%2==0){
+
+printf("Even");
+
+}else{
+
+printf("odd ");
+
+}
+ */
+
+
+
+// Leap year 
+/*
+int year ;
+printf("Enter your years");
+scanf("%d",&year);
+if(year %4 == 0){
+
+printf("Leap years");
+
+} else
+{
+printf("Not Leap years");
+
+}  
+ */   
+    
+    
+    
+    return 0;
+}
---
+++ C_full_notes/operator.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+  /*
+    Operator in c 
+    1. Arithmetic operator
+    2. Relational operator
+    3. Logical operator 
+    4.  Assignment operator
+    5. Increment & Decrement operator
+    6. conditional operator
+    7. Bitwise operator
+    8. special operator
+    
+
+
+*/
+    // post increment , Decrement
+    int a=2,b;
+    b=a++; // b=a--;
+    printf("%d \t %d", b, a);
+    
+    
+    // pre increments , Decrement
+   /*  int a=2;b;
+    b=++a; //b=--a;
+    printf("%d \t %d",b,a);
+    */
+    
+    return 0;
+}

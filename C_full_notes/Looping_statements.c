ce29e80cf23e64932634aa3eda75941a727f392f
MD SLAUDDIN <88940044+Mdslauddin@users.noreply.github.com>
2022-04-25 10:38:32 +0530

    Add files via upload

---
+++ C_full_notes/Looping_statements.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+
+int main()
+{
+
+ // Reverse printing 
+/*
+   int i=10;
+   while(i>= 1 ){
+
+  printf("%d \n",i);
+  i--;
+
+    }   */
+    
+  //    1  to 10 print
+  /*
+  int i=1;
+  while(i<=10){
+
+  printf("%d \n",i);
+  i++;
+
+}  
+    */
+    
+    
+   // Different while loop , do while loop
+/* 
+   // while loop
+  
+ int 1=10;
+  while(i>12) //  condition false exit
+{
+
+ printf("%d",&i);
+
+  } */
+    
+  //do while loop
+/* 
+int i=10;
+ do{
+
+ printf("%d",i);
+
+ }  while(i>12);
+    
+   */
+
+// The for loop  1 to 10 print
+/*
+  int i;
+for (i=1;i<=10;i++){
+
+printf("%d \n ", i);
+
+}
+
+*/
+
+// Table printing
+/*
+int num , c,i;
+ printf("Enter number for table printing : ");
+scanf("%d",&num);
+for(i=1;i<=10;i++){
+c=num*i;
+printf("%d * %d = %d \n ",num , i, c );
+
+} */
+
+ // Nested for loop star print
+/*
+ int i,j;
+ for(i=1;i<=5;i++){
+
+  for(j=1;j<=i;j++)
+{
+  printf("*");
+  
+ }  printf("\n");
+   
+
+}  */
+
+
+
+
+
+ 
+    return 0;
+}
---
+++ C_full_notes/switch_case.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+
+int main()
+{
+   // Decision makking (switch case)
+/* 
+ int ch ;
+  printf("what do you want to Learn \n");
+  printf("press 1 for java \n");
+  printf("press 2 for python \n");
+  printf("enter your choice : ");
+  scanf("%d",&ch);
+  switch(ch){
+  
+   case 1:
+   printf("\n you want to learn java ");
+   break;
+   case 2 :
+   printf("\n you want to learn python ");
+   break;
+   default:
+   printf("\n Wrong choice ");
+
+  }   */
+
+// switch  calculator 
+/*
+int a,b,c,ch;
+printf("press 1 for Addition \n");
+printf("press 2 for subtraction \n");
+printf("press 3 for Multiplication \n");
+printf("press 4 for Division \n ");
+printf("________________________\n");
+printf("Enter your choice : ");
+scanf("%d",&ch);
+printf("Enter two number : ");
+scanf("%d %d",&a,&b);
+switch(ch)
+{
+
+ case 1:
+  c=a+b;
+  printf("Result is = %d ",c);
+  break ;
+
+  case 2:
+  c=a-b;
+  printf("Result is = %d ",c);
+   break; 
+
+  case 3:
+  c=a*b;
+  printf("Result is = %d ",c);
+  break ;
+  
+  case 4:
+   c=a/b;
+  printf("Result is = %d ",c);
+  break;
+  
+  default:
+  printf("Wrong choice ");
+   
+
+}
+
+ */
+
+
+// vowels constonant check
+
+/*
+ char ch;
+printf("Enter any character : ");
+scanf("%c",&ch);
+switch(ch)
+{
+
+  case 'A':
+  case 'a':
+  printf("vowels");
+  break;
+
+  case 'E':
+  case 'e':
+  printf("vowels");
+  break;
+
+  case 'I':
+  case 'i':
+  printf("vowels");
+  break;
+  
+  case 'O':
+  case 'o':
+  printf("vowels");
+  break;
+ 
+  case 'U':
+  case 'u':
+  printf("vowels");
+  break;
+
+ 
+  default : 
+  printf("Consonant");
+ 
+}  */
+
+
+
+
+
+
+
+                                                                                  
+    return 0;
+}
